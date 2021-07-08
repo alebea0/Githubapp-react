73b035e7bf96520c90dd3160b2e926b077a61b93
alebea0 <17470044@campeche.tecnm.mx>
2021-07-08 12:55:10 -0500

    Lee nombre de usuario y obtiene perfil a traves de API de GitHub

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,42 @@
 import React from "react";
 import Card from './Card';
+import Form from './Form';
+
+const testData = [
+    {
+    id: 5025309,
+    name: "Jorge L Chuc Lopez",
+    avatar_url: "https://avatars.githubusercontent.com/u/5025309?v=4",
+    company: "GISoft"
+    },
+    {
+    id: 25519322,
+    name: "Jorge Chuc Lopez",
+    avatar_url: "https://avatars.githubusercontent.com/u/25519322?v=4",
+    company: "UACam"
+    },
+    {
+    id: 25960217,
+    name: "Jorge Luis Chuc Lopez",
+    avatar_url: "https://avatars.githubusercontent.com/u/81385825?v=4",
+    company: "ITCampeche"
+    }
+   ];
+
 class App extends React.Component {
-     constructor(props) {
+    constructor(props) {
         super(props);
+        this.state = {
+            profiles: [],
+            };
         }
+       
+        addNewProfile = (profileData) => {
+            console.log(profileData);
+            this.setState(prevState => ({
+                 profiles: [...prevState.profiles, profileData]
+            }));
+        };
 
      render() {
         return (
@@ -11,7 +44,8 @@ class App extends React.Component {
                 <div className="header">
                         {this.props.title}
                 </div>
-                <Card />
+                <Form onSubmit={this.addNewProfile}/>
+                <Card profiles={this.state.profiles} />
             </div>
         );
     }
